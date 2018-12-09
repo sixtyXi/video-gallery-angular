490c9e778d60c14a7cc8c8d8a1e4bfb2411a6409
Sergei Karaulov <sixty.xi@gmail.com>
2018-12-09 19:48:19 +0400

    Task-2-Components [DEV] add mocked data

---
+++ src/app/courses/course-list/mock-courses.ts
@@ -0,0 +1,39 @@
+import { VideoCourse } from '../../shared/VideoCourse.model';
+
+export const COURSES: VideoCourse[] = [
+  { 
+    id: 1,
+    title: 'Video Course 1',
+    creationDate: new Date(2018, 4, 23),
+    duration: 88,
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium soluta libero similique aperiam, velit inventore tempora sequi repudiandae sunt nesciunt!'
+  },
+  { 
+    id: 2,
+    title: 'Video Course 2',
+    creationDate: new Date(2018, 5, 10),
+    duration: 27,
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium soluta libero similique aperiam, velit inventore tempora sequi repudiandae sunt nesciunt!'
+  },
+  { 
+    id: 3,
+    title: 'Video Course 3',
+    creationDate: new Date(2018, 6, 14),
+    duration: 70,
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium soluta libero similique aperiam, velit inventore tempora sequi repudiandae sunt nesciunt!'
+  },
+  { 
+    id: 4,
+    title: 'Video Course 4',
+    creationDate: new Date(2018, 6, 16),
+    duration: 46,
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium soluta libero similique aperiam, velit inventore tempora sequi repudiandae sunt nesciunt!'
+  },
+  { 
+    id: 5,
+    title: 'Video Course 5',
+    creationDate: new Date(2018, 7, 21),
+    duration: 30,
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium soluta libero similique aperiam, velit inventore tempora sequi repudiandae sunt nesciunt!'
+  },
+]
