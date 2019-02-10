34487276029d7a29c2349045738cacc5f685f597
Sergei Karaulov <sixty.xi@gmail.com>
2019-02-10 18:30:18 +0400

    Task-8-Http [DEV] fix tests

--- src/app/courses/course-list/course-list.component.spec.ts
+++ src/app/courses/course-list/course-list.component.spec.ts
@@ -1,10 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CourseListComponent } from './course-list.component';
 import { COURSES } from '../../shared/mocks/mock-courses';
 import { OrderByPipe } from './order-by.pipe';
-import { FilterByTitlePipe } from './filter-by-title.pipe';
 
 @Component({ selector: 'app-course-item', template: '<div class="course-item-stub"></div>' })
 class CourseItemStubComponent {}
@@ -16,13 +16,9 @@ describe('CourseListComponent', () => {
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
-        declarations: [
-          CourseListComponent,
-          CourseItemStubComponent,
-          OrderByPipe,
-          FilterByTitlePipe
-        ],
-        schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+        declarations: [ CourseListComponent, CourseItemStubComponent, OrderByPipe ],
+        schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+        imports: [ HttpClientTestingModule ]
       }).compileComponents();
     })
   );
@@ -37,9 +33,10 @@ describe('CourseListComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should initialize #courses after #ngOnInit', () => {
+  it('should request #courses after #ngOnInit', () => {
+    spyOn(component, 'getList');
     component.ngOnInit();
-    expect(component.courses).toEqual(COURSES);
+    expect(component.getList).toHaveBeenCalled();
   });
 
   it('should invoke #onLoadMore on load more button click', () => {
@@ -47,16 +44,7 @@ describe('CourseListComponent', () => {
     const loadMoreBtn: HTMLButtonElement = hostElement.querySelector('.load-more-btn');
 
     spyOn(component, 'onLoadMore');
-    loadMoreBtn.dispatchEvent(new Event('click'));
+    loadMoreBtn.click();
     expect(component.onLoadMore).toHaveBeenCalled();
-  });
-
-  it('should render as many course-items as there are elements in #courses', () => {
-    const hostElement = fixture.nativeElement;
-    const mockCoursesAmount = COURSES.length;
-    let courseItemStubs: NodeList;
-
-    courseItemStubs = hostElement.querySelectorAll('.course-item-stub');
-    expect(courseItemStubs.length).toBe(mockCoursesAmount);
   });
 });
--- src/app/login/login-page/login-page.component.spec.ts
+++ src/app/login/login-page/login-page.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { LoginPageComponent } from './login-page.component';
 import { FormsModule } from '@angular/forms';
@@ -12,7 +13,7 @@ describe('LoginPageComponent', () => {
     async(() => {
       TestBed.configureTestingModule({
         declarations: [ LoginPageComponent ],
-        imports: [ FormsModule, RouterTestingModule ]
+        imports: [ FormsModule, RouterTestingModule, HttpClientTestingModule ]
       }).compileComponents();
     })
   );
