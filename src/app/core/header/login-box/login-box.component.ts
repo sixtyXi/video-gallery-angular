19d9e8ee1a4caebbd276044ea1e5b7979d47bfc6
Sergei Karaulov <sixty.xi@gmail.com>
2019-02-17 17:31:23 +0400

    Task-9-Rxjs [DEV] show user login in the header

--- src/app/core/header/login-box/login-box.component.ts
+++ src/app/core/header/login-box/login-box.component.ts
@@ -1,19 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth.service';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil, filter } from 'rxjs/operators';
+import { Person } from 'src/app/shared/models/Person.interface';
 
 @Component({
   selector: 'app-login-box',
   templateUrl: './login-box.component.html',
   styleUrls: [ './login-box.component.less' ]
 })
-export class LoginBoxComponent implements OnInit {
+export class LoginBoxComponent implements OnInit, OnDestroy {
+  public userLogin = '';
+  private ngUnsubscribe = new Subject();
+
   constructor(public authService: AuthService, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd), takeUntil(this.ngUnsubscribe))
+      .subscribe(() => {
+        if (this.authService.isAuth() && !this.userLogin) {
+          const id = localStorage.getItem('id');
+
+          this.authService
+            .getUserInfo(id)
+            .pipe(takeUntil(this.ngUnsubscribe))
+            .subscribe((res: Person) => {
+              this.userLogin = res.login;
+            });
+        }
+      });
+  }
 
   logOff() {
+    this.userLogin = '';
     this.authService.logOut();
     this.router.navigate([ 'login' ]);
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
