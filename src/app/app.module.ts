04d8c4eb571a8fe6f127fd1cb57b5ed1922b4029
Sergei Karaulov <sixty.xi@gmail.com>
2019-02-23 19:47:38 +0400

    Task-10-ngrx install ngrx dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,10 @@ import { CourseModule } from './course/course.module';
 import { PageNotFoundComponent } from './pageNotFound/page-not-found/page-not-found.component';
 import { AuthInterceptor } from './auth-interceptor';
 import { GlobalLoaderModule } from './globalLoader/globalLoader.module';
+import { EffectsModule } from '@ngrx/effects';
+import { reducers, metaReducers } from './store/reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AppEffects } from './store/effects/app.effects';
 
 @NgModule({
   declarations: [ AppComponent, PageNotFoundComponent ],
@@ -22,7 +27,9 @@ import { GlobalLoaderModule } from './globalLoader/globalLoader.module';
     LoginModule,
     CourseModule,
     GlobalLoaderModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    EffectsModule.forRoot([ AppEffects, AppEffects ])
   ],
   providers: [ { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true } ],
   bootstrap: [ AppComponent ]
